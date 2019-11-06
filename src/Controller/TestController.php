43a14ef51c9d0ae4cdb71b0cfb2c64f42afc02ba
Fuad Abdul Jabbar <fuad.abd.j@gmail.com>
2019-11-06 11:48:46 +0700

    remove unused variable

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,10 +13,10 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    public function index(\Swift_Mailer $mailer)
+    public function index()
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'greetings' => 'Hallo',
         ]);
     }
 
